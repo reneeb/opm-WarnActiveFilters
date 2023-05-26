e3f74bc6202b2432aa93ee50e074db5bbb5a3f9d
reneeb <info@perl-services.de>
2023-05-26 17:46:27 +0200

    prepare release

--- Kernel/Output/HTML/FilterContent/WarnActiveFilters.pm
+++ Kernel/Output/HTML/FilterContent/WarnActiveFilters.pm
@@ -1,6 +1,6 @@
 # --
 # Kernel/Output/HTML/FilterElementPost/WarnActiveFilters.pm
-# Copyright (C) 2017 - 2022 Perl-Services.de, https://www.perl-services.de/
+# Copyright (C) 2017 - 2023 Perl-Services.de, https://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
